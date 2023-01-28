844313b062d59d366a247f384757378b79665601
Alla <bakrlona@mail.ru>
2023-01-28 20:46:31 +0500

    Ex50

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿void PrintArray(int[,] matr) // Создание матрицы 
+{
+    for (int i = 0; i  < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int [,] matr) // Заполнение массива рандомными данными
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr [i,j] = new Random().Next(1,100);
+        }
+    }
+}
+
+int [,] array = new int [4,4];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine("выбор позиции в строке");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ввыбор позиции в столбце");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число для проверки");
+int numberTest = Convert.ToInt32(Console.ReadLine());
+
+if (array[linesVol,columnsVol] == numberTest)
+{
+    Console.WriteLine ($"Такое число - {numberTest} есть ");
+}
+else
+{
+    Console.WriteLine ($"Такое число отсутствует {numberTest}");
+}
